44dffd03a1c577e90c98c4d168d1f8a517b3335f
Sandeepvirk0590 <sandeep.cor14@gmail.com>
2023-07-05 21:27:45 -0400

    Learn to Add, commit and Push in gitHub

---
+++ src/learngithub/AddingCode.java
@@ -0,0 +1,16 @@
+package learngithub;
+
+public class AddingCode {
+
+	int num1;
+	int num2;
+	int add;
+
+	void addTheseNumbers() {
+
+		add = num1 + num2;
+
+		System.out.println("The total of two numbers is " + add);
+
+	}
+}
---
+++ src/learngithub/SharingCode.java
@@ -0,0 +1,17 @@
+package learngithub;
+
+public class SharingCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AddingCode sc = new AddingCode();
+		
+		sc.num1= 2;
+		sc.num2=5;
+		
+
+		sc.addTheseNumbers();
+	}
+
+}
