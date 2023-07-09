21315211f10cbf634784a7bc8c07497706c6f6c4
Sandeepvirk0590 <sandeep.cor14@gmail.com>
2023-07-09 17:30:31 -0400

    Loops Assignment Submitted

---
+++ src/loopsExample/LoopsMain.java
@@ -0,0 +1,24 @@
+package loopsExample;
+
+public class LoopsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PrintSuccessiveNumbers loop = new PrintSuccessiveNumbers();
+//		loop.number = 1;
+//		loop.finalNumber = 5;
+//		loop.printSuccessiveNumbers();
+		
+		PrintSuccessiveNumbers printNumber = new PrintSuccessiveNumbers();
+		printNumber.number = 10;
+		printNumber.finalNumber = 15;
+//		printNumber.printSuccssvNumbers();
+//		printNumber.printNumbersInReverse();
+		printNumber.printNumbersStopAt13();
+
+	}
+
+	}
+
+
